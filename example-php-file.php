8e32a5a48ad67a178295f2d67d75afdeeac3f654
Rich Plastow <rich@richplastow.com>
2017-02-07 16:19:45 +0000

    Create example-php-file.php

---
+++ example-php-file.php
@@ -0,0 +1,14 @@
+<?php
+
+//// About this script.
+$APP_NAME    = 'My Example App';
+$APP_VERSION = '1.0.0';
+$APP_AUTHOR  = 'richplastow';
+
+//// Enable error-reporting.
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+echo "<h1>$APP_NAME $APP_VERSION</h1>\n\n";
+
+?>
