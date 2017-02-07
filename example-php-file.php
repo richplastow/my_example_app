d3f9c0ea8551c93092e0d305270c60c35734a4c3
richplastow <rich@richplastow.com>
2017-02-07 19:00:00 +0000

    version bump

--- example-php-file.php
+++ example-php-file.php
@@ -2,13 +2,13 @@
 
 //// About this script.
 $APP_NAME    = 'My Example App';
-$APP_VERSION = '1.0.3';
+$APP_VERSION = '1.0.4';
 $APP_AUTHOR  = 'richplastow';
 
 //// Enable error-reporting.
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-echo "<h1>$APP_NAME $APP_VERSION It Works!!!</h1>\n\n";
+echo "<h1>$APP_NAME $APP_VERSION</h1>\n\n";
 
 ?>
